8dbec7ab204bfc090c44bf82ddc84672eb8ad03f
Chim Kan <designium@gmail.com>
2011-11-23 00:43:46 -0500

    Testing blog commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1aaf4147f426ecba7a859f3b62d4b1c65aca8d0757884957a9dd975b160bf32dcb584b49c43fc9f95787dff0c2f333c2b2bee04341e89f3766848b167779180f'
